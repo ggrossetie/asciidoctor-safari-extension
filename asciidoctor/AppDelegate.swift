3c10d6097e8210cc6c690bdbbabd21de467a4d1f
Guillaume Grossetie <ggrossetie@gmail.com>
2020-07-20 20:00:31 +0200

    Initial Commit

---
+++ asciidoctor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  asciidoctor
+//
+//  Created by Guillaume Grossetie on 20/07/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
